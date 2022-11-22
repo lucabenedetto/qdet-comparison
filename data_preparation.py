9108dacc23d84c2cce53725a664f5ee81120b424
Luca Benedetto <luca.benedetto93@gmail.com>
2022-11-22 15:54:52 +0000

    add assistments

--- data_preparation.py
+++ data_preparation.py
@@ -1,19 +1,21 @@
 from src.data_utils.race import prepare_racepp_dataset
 from src.data_utils.arc import prepare_arc_dataset
+from src.data_utils.am import prepare_assistments_dataset
 
 # TODO possibly: change the correct choice column so that it is an integer (to use ot as index for accessing the option)
 
-# RACE++
-race_data_dir = 'data/raw/RACE'
-race_c_data_dir = 'data/raw/race-c-master/data'
-prepare_racepp_dataset(race_data_dir, race_c_data_dir, 'data/processed')
-
-# ARC
-arc_data_dir = 'data/raw/ARC-V1-Feb2018'
-prepare_arc_dataset(arc_data_dir, 'data/processed')
+# # RACE++
+# race_data_dir = 'data/raw/RACE'
+# race_c_data_dir = 'data/raw/race-c-master/data'
+# prepare_racepp_dataset(race_data_dir, race_c_data_dir, 'data/processed')
+#
+# # ARC
+# arc_data_dir = 'data/raw/ARC-V1-Feb2018'
+# prepare_arc_dataset(arc_data_dir, 'data/processed')
 
 # ASSISTments
-pass  # TODO
+am_data_dir = 'data/interim/assistments'
+prepare_assistments_dataset(am_data_dir, 'data/processed')
 
 # CloudAcademy
 pass  # TODO

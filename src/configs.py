998b44446af29040b9c66c56fbb0067dc1d83c10
Luca Benedetto <luca.benedetto93@gmail.com>
2022-11-30 16:46:50 +0000

    add configs

--- src/configs.py
+++ src/configs.py
@@ -1,1 +1,28 @@
-READABILITY_AND_R2DE__LR = 'readability_and_r2de__LR'
+READ_AND_R2DE__LR = 'read_and_r2de__LR'
+
+# below, the ones still to implement
+LING_AND_R2DE__LR = 'ling_and_r2de__LR'
+LING_AND_R2DE__RF = 'ling_and_r2de__RF'
+LING_AND_READ_AND_R2DE__LR = 'ling_and_read_and_r2de__LR'
+LING_AND_READ_AND_R2DE__RF = 'ling_and_read_and_r2de__RF'
+LING_AND_READ__LR = 'ling_and_read__LR'
+LING_AND_READ__RF = 'ling_and_read__RF'
+LING__LINEAR_REGRESSOR = 'ling__linear_regressor'
+LING__RF = 'ling__RF'
+# MAJORITY = 'majority'  # TODO with different code
+# R2DE_Q_ALL = 'r2de_q_all'    # TODO with different code
+# R2DE_Q_CORRECT = 'r2de_q_correct'    # TODO with different code
+# R2DE_Q_ONLY = 'r2de_q_only'    # TODO with different code
+# READ_AND_R2DE__LR = 'read_and_r2de__LR'  # ALREADY DONE
+READ_AND_R2DE__RF = 'read_and_r2de__RF'
+READ__LINEAR_REGRESSOR = 'read__linear_regressor'
+READ__RF = 'read__RF'
+W2V_Q_ONLY_AND_LING_AND_R2DE__RF = 'w2v_q_only_and_ling_and_r2de__RF'
+W2V_Q_ONLY_AND_LING__RF = 'w2v_q_only_and_ling__RF'
+W2V_Q_ONLY__RF = 'w2v_q_only__RF'
+W2V_Q_ALL_AND_LING_AND_R2DE__RF = 'w2v_q_all_and_ling_and_r2de__RF'
+W2V_Q_ALL_AND_LING__RF = 'w2v_q_all_and_ling__RF'
+W2V_Q_ALL__RF = 'w2v_q_all__RF'
+W2V_Q_CORRECT_AND_LING_AND_R2DE__RF = 'w2v_q_correct_and_ling_and_r2de__RF'
+W2V_Q_CORRECT_AND_LING__RF = 'w2v_q_correct_and_ling__RF'
+W2V_Q_CORRECT__RF = 'w2v_q_correct__RF'

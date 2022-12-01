9a4171540af87b69d05121278f64c18156fa9a34
Luca Benedetto <luca.benedetto93@gmail.com>
2022-12-01 16:11:55 +0000

    refactor main

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import os
 import pickle
-
 from src.scripts_utils import (
-    get_dfs,
-    get_true_labels,
+    get_dataframes,
     get_mapper,
     get_model_by_config_and_dataset,
     randomized_cv_train,
@@ -12,22 +10,27 @@ from src.scripts_utils import (
 )
 from src.constants import RACE_PP, ARC, AM, OUTPUT_DIR
 from src.configs import *
+from src.constants import DATA_DIR
 
-dataset = RACE_PP
-config = READABILITY_AND_R2DE__LR
-print(config)
+dataset_name = RACE_PP
+feature_eng_config = READ
+regression_config = RF
 SEED = 42
 
-output_dir = os.path.join(OUTPUT_DIR, dataset, 'seed_' + str(SEED))
+config = get_config(feature_engineering_config=feature_eng_config, regression_config=regression_config)
+print(config)
+
+output_dir = os.path.join(OUTPUT_DIR, dataset_name, 'seed_' + str(SEED))
 
 # output_dir = get_output_dir(dataset)
-df_train, df_test = get_dfs(dataset)
+df_train, df_test = get_dataframes(dataset_name)
 
 # get model
-model = get_model_by_config_and_dataset(config, dataset)
+model = get_model_by_config_and_dataset(config, dataset_name, SEED)
 
 # train with randomized CV and save model
-scores = randomized_cv_train(model, df_train, SEED)
+dict_params = get_dict_params_by_config(config)
+scores = randomized_cv_train(model, dict_params, df_train, SEED)
 pickle.dump(model, open(os.path.join(output_dir, 'model_' + config + '.p'), 'wb'))
 
 # perform predictions and save them
@@ -35,11 +38,12 @@ y_pred_train, y_pred_test = get_predictions(model, df_test, df_train)
 pickle.dump(y_pred_test, open(os.path.join(output_dir, 'predictions_test_' + config + '.p'), 'wb'))
 pickle.dump(y_pred_train, open(os.path.join(output_dir, 'predictions_train_' + config + '.p'), 'wb'))
 
-my_mapper = get_mapper(dataset)
+my_mapper = get_mapper(dataset_name)
 converted_y_pred_test = [my_mapper(x) for x in y_pred_test]
 converted_y_pred_train = [my_mapper(x) for x in y_pred_train]
 
-# TODO this line below might be done once and for all in the data preparation steps
-y_true_test, y_true_train = get_true_labels(df_train, df_test, dataset)
+y_true_train = pickle.load(open(os.path.join(DATA_DIR, f'y_true_train_{dataset_name}.p'), 'rb'))
+y_true_dev = pickle.load(open(os.path.join(DATA_DIR, f'y_true_dev_{dataset_name}.p'), 'rb'))
+y_true_test = pickle.load(open(os.path.join(DATA_DIR, f'y_true_test_{dataset_name}.p'), 'rb'))
 
 evaluate_model(config, converted_y_pred_test, converted_y_pred_train, y_true_test, y_true_train, output_dir)

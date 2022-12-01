683cd2de9340dead117d66bc4f04084eee3a9454
Luca Benedetto <luca.benedetto93@gmail.com>
2022-12-01 17:24:03 +0000

    add new metrics

--- src/scripts_utils/_eval.py
+++ src/scripts_utils/_eval.py
@@ -1,28 +1,71 @@
 import os
 import pandas as pd
-from sklearn.metrics import accuracy_score, balanced_accuracy_score, f1_score
+from typing import List
+from sklearn.metrics import (
+    accuracy_score,
+    balanced_accuracy_score,
+    f1_score,
+    mean_absolute_error,
+    mean_squared_error,  # both MSE and RMSE
+    median_absolute_error,
+    r2_score,
+)
+from scipy.stats import (
+    spearmanr,
+    pearsonr,  # this only for AM since it assumes a gaussian distribution for both  # can also get the p-value
+    kendalltau,
+    # weightedtau,
+)
+
+F1 = 'f1_score'
+BAL_ACC = 'balanced_accuracy'
+ACC = 'accuracy'
+MAE = 'mean_absolute_error'
+MSE = 'mean_squared_error'
+RMSE = 'root_mean_squared_error'
+MED_AE = 'median_absolute_error'
+R2 = 'r2_score'
+SPEARMAN_R = 'spearman_rho'  # can get p-value
+PEARSON_R = 'pearson_rho'  # only AM
+KENDALL_TAU = 'kendall_tau'  # maybe
+# W_K_TAU = 'weighted_kendall_tau'  # maybe
 
 
-def evaluate_model(root_string, converted_y_pred_test, converted_y_pred_train, y_true_test, y_true_train, output_dir):
+def get_metrics(y_true, y_pred, discrete_regression: bool = False):
+    metrics = dict()
+    metrics[MAE] = mean_absolute_error(y_true, y_pred)
+    metrics[MSE] = mean_squared_error(y_true, y_pred, squared=True)
+    metrics[RMSE] = mean_squared_error(y_true, y_pred, squared=False)
+    metrics[MED_AE] = median_absolute_error(y_true, y_pred)
+    metrics[R2] = r2_score(y_true, y_pred)
+    metrics[SPEARMAN_R] = spearmanr(y_true, y_pred)
+    metrics[PEARSON_R] = pearsonr(y_true, y_pred)
+    metrics[KENDALL_TAU] = kendalltau(y_true, y_pred)
+    # metrics[W_K_TAU] = weightedtau(y_true, y_pred)  # not for us because "exchanges of high weight are more influential than exchanges of low weight"
+    if discrete_regression:
+        metrics[F1] = f1_score(y_true, y_pred, average='weighted')
+        metrics[BAL_ACC] = balanced_accuracy_score(y_true, y_pred)
+        metrics[ACC] = accuracy_score(y_true, y_pred)
+    return metrics
 
-    f1_test = f1_score(y_true_test, converted_y_pred_test, average='weighted')
-    bal_acc_test = balanced_accuracy_score(y_true_test, converted_y_pred_test)
-    acc_test = accuracy_score(y_true_test, converted_y_pred_test)
 
-    f1_train = f1_score(y_true_train, converted_y_pred_train, average='weighted')
-    bal_acc_train = balanced_accuracy_score(y_true_train, converted_y_pred_train)
-    acc_train = accuracy_score(y_true_train, converted_y_pred_train)
+def evaluate_model(
+        root_string: str,
+        y_pred_test: List[float],
+        y_pred_train: List[float],
+        y_true_test: List[float],
+        y_true_train: List[float],
+        output_dir: str,
+        discrete_regression: bool = False,
+):
 
-    out_df = pd.DataFrame([{
-        'train_f1': acc_train, 'train_balanced_acc': acc_train, 'train_acc': acc_train,
-        'test_f1': acc_test, 'test_balanced_acc': acc_test, 'test_acc': acc_test,
-    }])
+    metrics_test = get_metrics(y_true_test, y_pred_test, discrete_regression=discrete_regression)
+    metrics_train = get_metrics(y_true_train, y_pred_train, discrete_regression=discrete_regression)
+
+    metrics_dict = dict()
+    for metric in metrics_test.keys():
+        metrics_dict['test_' + metric] = metrics_test[metric]
+        metrics_dict['train_' + metric] = metrics_train[metric]
+    out_df = pd.DataFrame([{metrics_dict}])
     out_df.to_csv(os.path.join(output_dir, 'eval_metrics_' + root_string + '.csv'), index=False)
-
-    print("f1 test", f1_test)
-    print("balanced accuracy test", bal_acc_test)
-    print("accuracy test", acc_test)
-
-    print("f1 train", f1_train)
-    print("balanced accuracy train", bal_acc_train)
-    print("accuracy train", acc_train)
+    print(metrics_dict)

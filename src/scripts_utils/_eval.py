69b743abd122602474de75311f3266e661b5b763
Luca Benedetto <luca.benedetto93@gmail.com>
2022-12-02 10:52:22 +0000

    remove print from evaluate_model

--- src/scripts_utils/_eval.py
+++ src/scripts_utils/_eval.py
@@ -63,4 +63,4 @@ def evaluate_model(
         metrics_dict['train_' + metric] = metrics_train[metric]
     out_df = pd.DataFrame([metrics_dict])
     out_df.to_csv(os.path.join(output_dir, 'eval_metrics_' + root_string + '.csv'), index=False)
-    print(metrics_dict)
+    # print(metrics_dict)

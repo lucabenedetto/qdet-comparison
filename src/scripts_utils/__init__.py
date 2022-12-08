523b8f50c752a81dab578569dcbc3bae5c4f7a21
Luca Benedetto <luca.benedetto93@gmail.com>
2022-12-08 11:02:42 +0000

    add method for getting predictions r2de

--- src/scripts_utils/__init__.py
+++ src/scripts_utils/__init__.py
@@ -7,4 +7,9 @@ from src.scripts_utils._data_collection import (
 )
 from src.scripts_utils._eval import evaluate_model
 from src.scripts_utils._mapping_methods import mapper_race, get_mapper
-from src.scripts_utils._model import get_text2props_model_by_config_and_dataset, text2props_randomized_cv_train, get_predictions
+from src.scripts_utils._model import (
+    get_text2props_model_by_config_and_dataset,
+    text2props_randomized_cv_train,
+    get_predictions_text2props,
+    get_predictions_r2de,
+)

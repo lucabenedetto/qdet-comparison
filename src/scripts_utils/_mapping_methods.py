7994c1cf5b8de923ff329bb3404d82501e65a049
Luca Benedetto <luca.benedetto93@gmail.com>
2022-12-12 10:49:16 +0000

    add mapping method arc without balancing

--- src/scripts_utils/_mapping_methods.py
+++ src/scripts_utils/_mapping_methods.py
@@ -10,8 +10,19 @@ def mapper_race(x):
         return 2
 
 
-def get_mapper(dataset):
+def mapper_arc_without_balancing(x):
+    if x <= 3:
+        return 3
+    elif x >= 9:
+        return 9
+    else:
+        return round(x)
+
+
+def get_mapper(dataset: str, balancing: bool = False):
     if dataset in {RACE_PP, RACE_PP_4K, RACE_PP_8K, RACE_PP_12K}:
         return mapper_race
+    if dataset == ARC and not balancing:
+        return mapper_arc_without_balancing
     else:
         raise NotImplementedError

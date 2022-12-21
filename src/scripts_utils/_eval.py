3ddfe43b7a485e6be74939613638dd177b4b7ecc
Luca Benedetto <luca.benedetto93@gmail.com>
2022-12-21 11:44:03 +0000

    update evaluation metrics

--- src/scripts_utils/_eval.py
+++ src/scripts_utils/_eval.py
@@ -25,14 +25,18 @@ SPEARMAN_R = 'spearman_rho'  # can get p-value
 PEARSON_R = 'pearson_rho'  # only AM
 
 
-def get_metrics(y_true, y_pred, discrete_regression: bool = False):
+def get_metrics(y_true, y_pred, discrete_regression: bool = False, compute_correlation: bool = True):
     metrics = dict()
     metrics[MAE] = mean_absolute_error(y_true, y_pred)
     metrics[MSE] = mean_squared_error(y_true, y_pred, squared=True)
     metrics[RMSE] = mean_squared_error(y_true, y_pred, squared=False)
     metrics[R2] = r2_score(y_true, y_pred)
-    metrics[SPEARMAN_R] = spearmanr(y_true, y_pred).correlation
-    metrics[PEARSON_R] = pearsonr(y_true, y_pred).statistic
+    if compute_correlation:
+        metrics[SPEARMAN_R] = spearmanr(y_true, y_pred).correlation
+        metrics[PEARSON_R] = pearsonr(y_true, y_pred).statistic
+    else:
+        metrics[SPEARMAN_R] = None
+        metrics[PEARSON_R] = None
     if discrete_regression:
         metrics[F1] = f1_score(y_true, y_pred, average='weighted')
         metrics[BAL_ACC] = balanced_accuracy_score(y_true, y_pred)
@@ -52,10 +56,15 @@ def evaluate_model(
         y_true_train: List[float],
         output_dir: str,
         discrete_regression: bool = False,
+        compute_correlation: bool = True,
 ):
 
-    metrics_test = get_metrics(y_true_test, y_pred_test, discrete_regression=discrete_regression)
-    metrics_train = get_metrics(y_true_train, y_pred_train, discrete_regression=discrete_regression)
+    metrics_test = get_metrics(
+        y_true_test, y_pred_test, discrete_regression=discrete_regression, compute_correlation=compute_correlation
+    )
+    metrics_train = get_metrics(
+        y_true_train, y_pred_train, discrete_regression=discrete_regression, compute_correlation=compute_correlation
+    )
 
     metrics_dict = dict()
     for metric in metrics_test.keys():

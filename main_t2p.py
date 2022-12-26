fed8446b20244a98a15090a77c27fe4afa8ce1d4
Luca Benedetto <luca.benedetto93@gmail.com>
2022-12-26 22:14:39 +0000

    clean script

--- main_t2p.py
+++ main_t2p.py
@@ -15,23 +15,33 @@ from src.configs import *
 
 nltk.download('averaged_perceptron_tagger')
 
-LIST_DATASET_NAMES = [RACE_PP]
-LIST_FEATURE_ENG_CONFIGS = [W2V_Q_ALL, W2V_Q_CORRECT, W2V_Q_ONLY]
-REGRESSION_CONFIG = LR
+LIST_DATASET_NAMES = [RACE_PP, ARC, AM, RACE_PP_4K, RACE_PP_8K, RACE_PP_12K]
+LIST_FEATURE_ENG_CONFIGS = [
+    LING,
+    READ,
+    W2V_Q_ONLY,
+    W2V_Q_ALL,
+    W2V_Q_CORRECT,
+    LING_AND_READ,
+    W2V_Q_ONLY_AND_LING,
+    W2V_Q_ALL_AND_LING,
+    W2V_Q_CORRECT_AND_LING,
+]
+REGRESSION_CONFIG = RF
 RANDOM_SEEDS = [0, 1, 2, 3, 4]
 N_ITER = 20
 N_JOBS = 10
 
-for dataset_name in LIST_DATASET_NAMES:
+for dataset in LIST_DATASET_NAMES:
 
     # dataset-related variables
-    df_train, df_test = get_dataframes_text2props(dataset_name)
-    my_mapper = get_mapper(dataset_name)
-    discrete_regression = dataset_name in {RACE_PP, RACE_PP_4K, RACE_PP_8K, RACE_PP_12K, ARC}
+    df_train, df_test = get_dataframes_text2props(dataset)
+    my_mapper = get_mapper(dataset)
+    discrete_regression = dataset in {RACE_PP, RACE_PP_4K, RACE_PP_8K, RACE_PP_12K, ARC}
 
-    y_true_train = pickle.load(open(os.path.join(DATA_DIR, f'y_true_train_{dataset_name}.p'), 'rb'))
-    y_true_dev = pickle.load(open(os.path.join(DATA_DIR, f'y_true_dev_{dataset_name}.p'), 'rb'))
-    y_true_test = pickle.load(open(os.path.join(DATA_DIR, f'y_true_test_{dataset_name}.p'), 'rb'))
+    y_true_train = pickle.load(open(os.path.join(DATA_DIR, f'y_true_train_{dataset}.p'), 'rb'))
+    y_true_dev = pickle.load(open(os.path.join(DATA_DIR, f'y_true_dev_{dataset}.p'), 'rb'))
+    y_true_test = pickle.load(open(os.path.join(DATA_DIR, f'y_true_test_{dataset}.p'), 'rb'))
 
     for feature_eng_config in LIST_FEATURE_ENG_CONFIGS:
 
@@ -40,12 +50,12 @@ for dataset_name in LIST_DATASET_NAMES:
         dict_params = get_dict_params_by_config(config)
 
         for random_seed in RANDOM_SEEDS:
-            print(f'{dataset_name} - {config} - seed_{random_seed}')
+            print(f'{dataset} - {config} - seed_{random_seed}')
 
-            output_dir = os.path.join(OUTPUT_DIR, dataset_name, 'seed_' + str(random_seed))
+            output_dir = os.path.join(OUTPUT_DIR, dataset, 'seed_' + str(random_seed))
 
             # get model
-            model = get_text2props_model_by_config_and_dataset(config, dataset_name, random_seed)
+            model = get_text2props_model_by_config_and_dataset(config, dataset, random_seed)
 
             # train with randomized CV and save model
             scores = text2props_randomized_cv_train(model, dict_params, df_train, random_seed, n_iter=N_ITER, n_jobs=N_JOBS)

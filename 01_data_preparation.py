fd683432a15feb2de5acdf96a01d8f2ac7ff30ec
Luca Benedetto <luca.benedetto93@gmail.com>
2022-12-27 11:07:22 +0000

    add preparation arc_balanced_data

--- 01_data_preparation.py
+++ 01_data_preparation.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from src.constants import DATA_DIR, DEV, TEST, TRAIN
 from src.data_utils.race import prepare_racepp_dataset
 from src.data_utils.arc import prepare_arc_dataset
@@ -27,6 +29,18 @@ arc_data_dir = 'data/raw/ARC-V1-Feb2018'
 df_train, df_dev, df_test = prepare_arc_dataset(arc_data_dir, 'data/processed')
 convert_to_r2de_format_and_store_data(df_train, df_dev, df_test, DATA_DIR, 'arc')
 convert_to_text2props_format_and_store_data(df_train, df_dev, df_test, DATA_DIR, 'arc')
+
+print("DOING ARC Balanced")
+balanced_df_train = pd.DataFrame(columns=df_train.columns)
+for diff in range(3, 10):
+    if diff in {5, 8}:
+        balanced_df_train = pd.concat([balanced_df_train, df_train[df_train['difficulty'] == diff].sample(n=500)], ignore_index=True)
+    else:
+        balanced_df_train = pd.concat([balanced_df_train, df_train[df_train['difficulty'] == diff]], ignore_index=True)
+balanced_df_train = balanced_df_train.sample(frac=1.0)
+convert_to_r2de_format_and_store_data(balanced_df_train, df_dev, df_test, DATA_DIR, 'arc_balanced')
+convert_to_text2props_format_and_store_data(balanced_df_train, df_dev, df_test, DATA_DIR, 'arc_balanced')
+
 
 # ASSISTments
 print("[INFO] DOING AM...")

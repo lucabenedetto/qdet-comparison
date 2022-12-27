4ad84b96b26f15f37be6efe1fb6485ae9b0a6b0b
Luca Benedetto <luca.benedetto93@gmail.com>
2022-12-27 11:09:38 +0000

    add arc balanced and remove arc grouped

--- src/scripts_utils/_mapping_methods.py
+++ src/scripts_utils/_mapping_methods.py
@@ -1,4 +1,4 @@
-from src.constants import RACE_PP, ARC, AM, RACE_PP_4K, RACE_PP_8K, RACE_PP_12K
+from src.constants import RACE_PP, ARC, ARC_BALANCED, AM, RACE_PP_4K, RACE_PP_8K, RACE_PP_12K
 
 
 def mapper_race(x):
@@ -14,7 +14,7 @@ def mapper_am(x):
     return x
 
 
-def mapper_arc_without_grouping(x):
+def mapper_arc(x):
     if x < 3.5:
         return 3
     elif x >= 8.5:
@@ -23,27 +23,11 @@ def mapper_arc_without_grouping(x):
         return round(x)
 
 
-def mapper_arc_with_grouping(x):
-    if x < 4.5:
-        return 3
-    elif x < 5.5:
-        return 5
-    elif x < 7.5:
-        return 7
-    elif x < 8.5:
-        return 8
-    else:
-        return 9
-# 3 & 4 - 115+379=494 | 5 - 690 | 6 & 7 - 99+406 = 505 | 8 - 1375 | 9 - 294
-
-
-def get_mapper(dataset: str, grouping: bool = False):
+def get_mapper(dataset: str):
     if dataset in {RACE_PP, RACE_PP_4K, RACE_PP_8K, RACE_PP_12K}:
         return mapper_race
-    if dataset == ARC and not grouping:
-        return mapper_arc_without_grouping
-    if dataset == ARC and grouping:
-        return mapper_arc_with_grouping
+    if dataset in {ARC, ARC_BALANCED}:
+        return mapper_arc
     if dataset == AM:
         return mapper_am
     else:

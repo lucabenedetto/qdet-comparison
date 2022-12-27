93a566e893b061c56b564576df8db612cd8d5c3d
Luca Benedetto <luca.benedetto93@gmail.com>
2022-12-27 13:25:20 +0000

    refactor and clean

--- 02_mapping_for_transformers.py
+++ 02_mapping_for_transformers.py
@@ -23,8 +23,7 @@ AS_TYPE_DICT = {CORRECT: bool, DESCRIPTION: str, ANS_ID: str, QUESTION_ID: str}
 
 
 def main_tf():
-    # for dataset_name in [RACE_PP, RACE_PP_4K, RACE_PP_8K, RACE_PP_12K, ARC, ARC_BALANCED, AM]:
-    for dataset_name in [ARC_BALANCED]:
+    for dataset_name in [RACE_PP, RACE_PP_4K, RACE_PP_8K, RACE_PP_12K, ARC, ARC_BALANCED, AM]:
 
         df_train = pd.read_csv(os.path.join(DATA_DIR, f'{dataset_name}_train.csv'))[DF_COLS]
         if dataset_name in {RACE_PP, RACE_PP_4K, RACE_PP_8K, RACE_PP_12K}:
@@ -36,19 +35,27 @@ def main_tf():
 
         ans_texts_df = pd.DataFrame(columns=[CORRECT, DESCRIPTION, ANS_ID, QUESTION_ID])
         print(f"Doing dataset {dataset_name}")
+        skip_ans_texts = dataset_name in {AM}
+
         print("Doing train...")
-        ans_texts_df = store_text_difficulty_df_and_return_updated_answers_text_df(dataset_name, df_train, 'train', ans_texts_df)
+        text_difficulty_df, ans_texts_df = get_text_difficulty_and_answer_texts(df_train, ans_texts_df, skip_ans_texts)
+        text_difficulty_df.to_csv(os.path.join(DATA_DIR, 'for_tf', f'text_difficulty_{dataset_name}_train.csv'), index=False)
+
         print("Doing test...")
-        ans_texts_df = store_text_difficulty_df_and_return_updated_answers_text_df(dataset_name, df_test, 'test', ans_texts_df)
+        text_difficulty_df, ans_texts_df = get_text_difficulty_and_answer_texts(df_test, ans_texts_df, skip_ans_texts)
+        text_difficulty_df.to_csv(os.path.join(DATA_DIR, 'for_tf', f'text_difficulty_{dataset_name}_test.csv'), index=False)
+
         print("Doing dev...")
-        ans_texts_df = store_text_difficulty_df_and_return_updated_answers_text_df(dataset_name, df_dev, 'dev', ans_texts_df)
-        if dataset_name not in {AM}:
-            ans_texts_df.to_csv(os.path.join(DATA_DIR, 'for_tf', 'answers_texts_{dataset_name}.csv'), index=False)
+        text_difficulty_df, ans_texts_df = get_text_difficulty_and_answer_texts(df_dev, ans_texts_df, skip_ans_texts)
+        text_difficulty_df.to_csv(os.path.join(DATA_DIR, 'for_tf', f'text_difficulty_{dataset_name}_dev.csv'), index=False)
+
+        if not skip_ans_texts:
+            ans_texts_df.to_csv(os.path.join(DATA_DIR, 'for_tf', f'answers_texts_{dataset_name}.csv'), index=False)
 
 
-def store_text_difficulty_df_and_return_updated_answers_text_df(dataset, df, split, answers_text_df):
+def get_text_difficulty_and_answer_texts(df, answers_text_df, skip_ans_texts):
     text_difficulty_df = pd.DataFrame(columns=[DESCRIPTION, QUESTION_ID, DIFFICULTY])
-    if dataset in {AM}:
+    if skip_ans_texts:
         for question, context, q_id, difficulty in df[[QUESTION, CONTEXT, Q_ID, DIFFICULTY]].values:
             text_difficulty_df = pd.concat(
                 [text_difficulty_df, get_new_row_text_difficulty_df(q_id, question, context, difficulty)],
@@ -67,8 +74,7 @@ def store_text_difficulty_df_and_return_updated_answers_text_df(dataset, df, spl
                 [text_difficulty_df, get_new_row_text_difficulty_df(q_id, question, context, difficulty)],
                 ignore_index=True
             )
-    text_difficulty_df.to_csv(os.path.join(DATA_DIR, 'for_tf', f'text_difficulty_{dataset}_{split}.csv'), index=False)
-    return answers_text_df
+    return text_difficulty_df, answers_text_df
 
 
 def get_new_rows_answers_text_df(correct_ans, q_id, options):

030f327bc51d300c0f4d9dd5fc008474b6400fc1
Luca Benedetto <luca.benedetto93@gmail.com>
2022-12-28 12:23:48 +0000

    fix output filename

--- 05_plot_distribution_estimated_difficulty_arc_balanced.py
+++ 05_plot_distribution_estimated_difficulty_arc_balanced.py
@@ -10,18 +10,18 @@ LING_N_READ = 'Ling. & Read.'
 
 # BERT_QA
 df = pd.read_csv('data/transformers_predictions/arc_balanced/predictions_test_BERT_question_all_0.csv')
-plot_violinplot_arc(df, BERT_QA, output_filename='bert_qa')
+plot_violinplot_arc(df, BERT_QA, output_filename='balanced_bert_qa')
 
 # DISTILBERT_QA
 df = pd.read_csv('data/transformers_predictions/arc_balanced/predictions_test_DistilBERT_question_all_0.csv')
-plot_violinplot_arc(df, DISTILBERT_QA, output_filename='distilbert_qa')
+plot_violinplot_arc(df, DISTILBERT_QA, output_filename='balanced_distilbert_qa')
 
 # R2DE_QA
 df = pd.read_csv('data/processed/arc_test.csv')
 df['predicted_difficulty'] = pickle.load(open('output/arc_balanced/seed_0/predictions_test_r2de_encoding_2.p', 'rb'))
-plot_violinplot_arc(df, R2DE_QA, output_filename='r2de_qa')
+plot_violinplot_arc(df, R2DE_QA, output_filename='balanced_r2de_qa')
 
 # LING_N_READ
 df = pd.read_csv('data/processed/arc_test.csv')
 df['predicted_difficulty'] = pickle.load(open('output/arc_balanced/seed_0/predictions_test_ling_and_read__RF.p', 'rb'))
-plot_violinplot_arc(df, LING_N_READ, output_filename='ling_n_read')
+plot_violinplot_arc(df, LING_N_READ, output_filename='balanced_ling_n_read')

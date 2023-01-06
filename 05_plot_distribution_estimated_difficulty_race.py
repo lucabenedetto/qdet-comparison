79acf4357a864b327be87ba6c3563fe5e0f15d0a
Luca Benedetto <luca.benedetto93@gmail.com>
2023-01-06 17:23:55 +0000

    rafactor script to plot all datasets from single script

--- 05_plot_distribution_estimated_difficulty_race.py
+++ 05_plot_distribution_estimated_difficulty_race.py
@@ -1,16 +1,21 @@
 import pandas as pd
 import pickle
 
-from src.plot_utils import plot_violinplot_race
+from src.plot_utils import plot_violinplot_race, plot_violinplot_arc, plot_hexbin_am
 
 
-LING_RF = 'Ling.'
-# READ_RF = 'Read.'
-# W2V_QA = r'W2V $Q_A$'
-# R2DE_QC = r'R2DE $Q_C$'
-DISTILBERT_QA = 'DistilBERT $Q_A$'
 BERT_QA = 'BERT $Q_A$'
+BERT_QO = 'BERT $Q_O$'
+DISTILBERT_QA = 'DistilBERT $Q_A$'
+DISTILBERT_QO = 'DistilBERT $Q_O$'
+LING_RF = 'Ling.'
+R2DE_QA = r'TF-IDF $Q_A$'
+R2DE_QO = r'TF-IDF $Q_O$'
+W2V_QO = r'W2V $Q_O$'
 W2V_QO_N_LING = r'W2V $Q_{Only}$ & Ling.'
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# RACE
 
 # BERT_QA
 df = pd.read_csv('data/transformers_predictions/race_pp/predictions_test_BERT_3_question_all_0.csv')
@@ -25,22 +30,34 @@ df = pd.read_csv('data/processed/race_pp_test.csv')
 df['predicted_difficulty'] = pickle.load(open('output/race_pp/seed_0/predictions_test_ling__RF.p', 'rb'))
 plot_violinplot_race(df, title=LING_RF, output_filename='ling')
 
-# READ_RF
-# df = pd.read_csv('data/processed/race_pp_test.csv')
-# df['predicted_difficulty'] = pickle.load(open('output/race_pp/seed_0/predictions_test_read__RF.p', 'rb'))
-# plot_violinplot_race(df, title=READ_RF)
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# ARC
 
-# W2V_QA
-# df = pd.read_csv('data/processed/race_pp_test.csv')
-# df['predicted_difficulty'] = pickle.load(open('output/race_pp/seed_0/predictions_test_w2v_q_all__RF.p', 'rb'))
-# plot_violinplot_race(df, title=W2V_QA)
+# BERT_QA
+df = pd.read_csv('data/transformers_predictions/arc_balanced/predictions_test_BERT_question_all_0.csv')
+plot_violinplot_arc(df, BERT_QA, output_filename='balanced_bert_qa')
 
-# R2DE_QC
-# df = pd.read_csv('data/processed/race_pp_test.csv')
-# df['predicted_difficulty'] = pickle.load(open('output/race_pp/seed_0/predictions_test_r2de_encoding_1.p', 'rb'))
-# plot_violinplot_race(df, title=R2DE_QC)
+# DISTILBERT_QA
+df = pd.read_csv('data/transformers_predictions/arc_balanced/predictions_test_DistilBERT_question_all_0.csv')
+plot_violinplot_arc(df, DISTILBERT_QA, output_filename='balanced_distilbert_qa')
 
-# W2V_QO_N_LING
-df = pd.read_csv('data/processed/race_pp_test.csv')
-df['predicted_difficulty'] = pickle.load(open('output/race_pp/seed_0/predictions_test_w2v_q_only_and_ling__RF.p', 'rb'))
-plot_violinplot_race(df, title=W2V_QO_N_LING, output_filename='w2v_q_only_n_ling')
+# TF-IDF_QA
+df = pd.read_csv('data/processed/arc_test.csv')
+df['predicted_difficulty'] = pickle.load(open('output/arc_balanced/seed_0/predictions_test_r2de_encoding_2.p', 'rb'))
+plot_violinplot_arc(df, R2DE_QA, output_filename='balanced_r2de_qa')
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# AM
+
+# BERT_QO
+df = pd.read_csv('data/transformers_predictions/am/predictions_test_BERT_question_only_0.csv')
+plot_hexbin_am(df, title=BERT_QO, output_filename='bert_qo')
+
+# DISTILBERT_QO
+df = pd.read_csv('data/transformers_predictions/am/predictions_test_DistilBERT_question_only_0.csv')
+plot_hexbin_am(df, title=DISTILBERT_QO, output_filename='distilbert_qo')
+
+# W2V_QO
+df = pd.read_csv('data/processed/am_test.csv')
+df['predicted_difficulty'] = pickle.load(open('output/am/seed_0/predictions_test_w2v_q_only__RF.p', 'rb'))
+plot_hexbin_am(df, title=W2V_QO, output_filename='w2v_qo')

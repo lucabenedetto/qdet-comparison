a021acaa8692fbd28d31496019651f5925da804c
Luca Benedetto <luca.benedetto93@gmail.com>
2023-01-10 16:17:24 +0000

    rename method, making it private

--- src/data_utils/mapping_tranformers.py
+++ src/data_utils/mapping_tranformers.py
@@ -15,7 +15,7 @@ def get_text_difficulty_and_answer_texts(df, answers_text_df, skip_ans_texts):
     if skip_ans_texts:
         for question, context, q_id, difficulty in df[[QUESTION, CONTEXT, Q_ID, DIFFICULTY]].values:
             text_difficulty_df = pd.concat(
-                [text_difficulty_df, get_new_row_text_difficulty_df(q_id, question, context, difficulty)],
+                [text_difficulty_df, _get_new_row_text_difficulty_df(q_id, question, context, difficulty)],
                 ignore_index=True
             )
     else:
@@ -23,18 +23,18 @@ def get_text_difficulty_and_answer_texts(df, answers_text_df, skip_ans_texts):
             answers_text_df = pd.concat(
                 [
                     answers_text_df.astype(AS_TYPE_DICT),
-                    get_new_rows_answers_text_df(correct_option, q_id, [opt0, opt1, opt2, opt3]).astype(AS_TYPE_DICT)
+                    _get_new_rows_answers_text_df(correct_option, q_id, [opt0, opt1, opt2, opt3]).astype(AS_TYPE_DICT)
                 ],
                 ignore_index=True
             )
             text_difficulty_df = pd.concat(
-                [text_difficulty_df, get_new_row_text_difficulty_df(q_id, question, context, difficulty)],
+                [text_difficulty_df, _get_new_row_text_difficulty_df(q_id, question, context, difficulty)],
                 ignore_index=True
             )
     return text_difficulty_df, answers_text_df
 
 
-def get_new_rows_answers_text_df(correct_ans, q_id, options):
+def _get_new_rows_answers_text_df(correct_ans, q_id, options):
     out_df = pd.DataFrame(columns=[CORRECT, DESCRIPTION, ANS_ID, QUESTION_ID])
     for idx, option in enumerate(options):
         new_row = pd.DataFrame([{CORRECT: idx == correct_ans, DESCRIPTION: option, ANS_ID: idx, QUESTION_ID: q_id}])
@@ -42,6 +42,6 @@ def get_new_rows_answers_text_df(correct_ans, q_id, options):
     return out_df
 
 
-def get_new_row_text_difficulty_df(q_id, question, context, difficulty):
+def _get_new_row_text_difficulty_df(q_id, question, context, difficulty):
     context = '' if type(context) != str else context + ' '
     return pd.DataFrame([{DESCRIPTION: context + question, QUESTION_ID: q_id, DIFFICULTY: difficulty,}])

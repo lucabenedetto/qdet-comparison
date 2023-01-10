bd68efe0d13a7768ce73ebff6f9ee42cc005aeb1
Luca Benedetto <luca.benedetto93@gmail.com>
2023-01-10 16:12:57 +0000

    rename file with methods for r2de mapping

--- data_preparation.py
+++ data_preparation.py
@@ -6,7 +6,7 @@ from src.data_utils.race import prepare_racepp_dataset
 from src.data_utils.arc import prepare_arc_dataset
 from src.data_utils.am import prepare_assistments_dataset
 from src.data_utils.text2props_mapping import convert_to_text2props_format_and_store_data
-from src.data_utils.r2de_mapping import convert_to_r2de_format_and_store_data
+from src.data_utils.mapping_r2de import convert_to_r2de_format_and_store_data
 
 # RACE++
 print("DOING RACE++")

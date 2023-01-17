dc37af4a11d2b3bde7c73ee7a2e3ec54c24f3014
Luca Benedetto <luca.benedetto93@gmail.com>
2023-01-17 15:29:54 +0000

    add requirements and setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='qdet_utils',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Utils for performing QDET',
+    author='Luca Benedetto',
+    license='',
+    install_requires=[
+        "gensim==4.2.0",
+        "nltk==3.7",
+        "numpy==1.23.4",
+        "pandas==1.5.1",
+        "pyirt==0.3.4",
+        "scikit-learn==1.1.3",
+        "scipy==1.9.3",
+        "seaborn==0.12.1",
+        "textstat==0.7.3",
+        "matplotlib==3.6.2",
+    ],
+)

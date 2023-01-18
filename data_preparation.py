743c70bb57f0130d3620b7214b34565b3fc96584
Luca Benedetto <luca.benedetto93@gmail.com>
2023-01-18 16:09:55 +0000

    refactor dataset names

--- data_preparation.py
+++ data_preparation.py
@@ -2,7 +2,12 @@ import logging
 import os
 import pandas as pd
 
-from qdet_utils.constants import DATA_DIR
+from qdet_utils.constants import (
+    DATA_DIR,
+    RACE_PP,
+    ARC,
+    AM,
+)
 from qdet_utils.data_manager import (
     RaceDatamanager,
     ArcDataManager,
@@ -19,27 +24,28 @@ def main():
     logger.info("Starting preparation RACE++")
     race_data_dir = 'data/raw/RACE'
     race_c_data_dir = 'data/raw/race-c-master/data'
-    racepp_dm = RaceDatamanager()
-    dataset = racepp_dm.get_racepp_dataset(race_data_dir, race_c_data_dir, DATA_DIR)
+    race_pp_dm = RaceDatamanager()
+    dataset = race_pp_dm.get_racepp_dataset(race_data_dir, race_c_data_dir, DATA_DIR)
     # whole RACE++
-    racepp_dm.convert_to_r2de_format_and_store_dataset(dataset, DATA_DIR, 'race_pp')
-    racepp_dm.convert_to_text2props_format_and_store_dataset(dataset, DATA_DIR, 'race_pp')
-    racepp_dm.convert_to_transformers_format_and_store_dataset(dataset, DATA_DIR, 'race_pp', skip_answers_texts=False)
+    race_pp_dm.convert_to_r2de_format_and_store_dataset(dataset, DATA_DIR, RACE_PP)
+    race_pp_dm.convert_to_text2props_format_and_store_dataset(dataset, DATA_DIR, RACE_PP)
+    race_pp_dm.convert_to_transformers_format_and_store_dataset(dataset, DATA_DIR, RACE_PP, skip_answers_texts=False)
     # sub-sampled datasets
     for training_size in [4_000, 8_000, 12_000]:
-        subsampled_dataset = racepp_dm.get_subsampled_racepp_dataset(dataset, training_size, DATA_DIR)
-        racepp_dm.convert_to_r2de_format_and_store_dataset(subsampled_dataset, DATA_DIR, f'race_pp_{training_size}')
-        racepp_dm.convert_to_text2props_format_and_store_dataset(subsampled_dataset, DATA_DIR, f'race_pp_{training_size}')
-        racepp_dm.convert_to_transformers_format_and_store_dataset(subsampled_dataset, DATA_DIR, f'race_pp_{training_size}', skip_answers_texts=False)
+        sub_sampled_dataset = race_pp_dm.get_subsampled_racepp_dataset(dataset, training_size, DATA_DIR)
+        dataset_name = f'{RACE_PP}_{training_size}'
+        race_pp_dm.convert_to_r2de_format_and_store_dataset(sub_sampled_dataset, DATA_DIR, dataset_name)
+        race_pp_dm.convert_to_text2props_format_and_store_dataset(sub_sampled_dataset, DATA_DIR, dataset_name)
+        race_pp_dm.convert_to_transformers_format_and_store_dataset(sub_sampled_dataset, DATA_DIR, dataset_name, False)
 
     # ARC
     logger.info("Starting preparation ARC")
     arc_data_dir = 'data/raw/ARC-V1-Feb2018'
     arc_dm = ArcDataManager()
     dataset = arc_dm.get_arc_dataset(arc_data_dir, DATA_DIR)
-    arc_dm.convert_to_r2de_format_and_store_dataset(dataset, DATA_DIR, 'arc')
-    arc_dm.convert_to_text2props_format_and_store_dataset(dataset, DATA_DIR, 'arc')
-    arc_dm.convert_to_transformers_format_and_store_dataset(dataset, DATA_DIR, 'arc', skip_answers_texts=False)
+    arc_dm.convert_to_r2de_format_and_store_dataset(dataset, DATA_DIR, ARC)
+    arc_dm.convert_to_text2props_format_and_store_dataset(dataset, DATA_DIR, ARC)
+    arc_dm.convert_to_transformers_format_and_store_dataset(dataset, DATA_DIR, ARC, skip_answers_texts=False)
 
     # TODO
     # logger.info("Starting preparation ARC Balanced")
@@ -61,9 +67,9 @@ def main():
     am_data_dir = 'data/interim/assistments'
     am_dm = AmDataManager()
     dataset = am_dm.get_assistments_dataset(am_data_dir, DATA_DIR)
-    am_dm.convert_to_r2de_format_and_store_dataset(dataset, DATA_DIR, 'am')
-    am_dm.convert_to_text2props_format_and_store_dataset(dataset, DATA_DIR, 'am')
-    am_dm.convert_to_transformers_format_and_store_dataset(dataset, DATA_DIR, 'am', skip_answers_texts=True)
+    am_dm.convert_to_r2de_format_and_store_dataset(dataset, DATA_DIR, AM)
+    am_dm.convert_to_text2props_format_and_store_dataset(dataset, DATA_DIR, AM)
+    am_dm.convert_to_transformers_format_and_store_dataset(dataset, DATA_DIR, AM, skip_answers_texts=True)
 
 
 if __name__ == "__main__":

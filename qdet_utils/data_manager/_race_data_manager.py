086910fea45129d94f2b918045f5764e9576bcba
Luca Benedetto <luca.benedetto93@gmail.com>
2023-01-18 13:14:43 +0000

    refactor

--- qdet_utils/data_manager/_race_data_manager.py
+++ qdet_utils/data_manager/_race_data_manager.py
@@ -1,3 +1,4 @@
+from typing import Dict
 import pandas as pd
 import json
 import os
@@ -20,32 +21,48 @@ from qdet_utils.constants import (
     DEV,
     TEST,
     TRAIN,
-
 )
 
 from ._data_manager import DataManager
 
 
 class RaceDatamanager(DataManager):
+    ANSWERS = 'answers'
+    OPTIONS = 'options'
+    QUESTIONS = 'questions'
+    ARTICLE = 'article'
+    ID = 'id'
+
     HIGH = 'high'
     MIDDLE = 'middle'
     COLLEGE = 'college'
 
-    LEVEL_TO_DIFFICULTY_MAP = {MIDDLE: 0, HIGH: 1, COLLEGE: 2}
+    LEVEL_TO_INT_DIFFICULTY_MAP = {MIDDLE: 0, HIGH: 1, COLLEGE: 2}
 
-    def get_racepp_dataset(self, race_data_dir, race_c_data_dir, output_data_dir, save_dataset=True):
+    def get_racepp_dataset(
+            self,
+            race_data_dir: str,
+            race_c_data_dir: str,
+            output_data_dir: str,
+            save_dataset: bool = True,
+    ) -> Dict[str, pd.DataFrame]:
         dataset = dict()
         for split in [TRAIN, DEV, TEST]:
             df_race = self.get_raw_race_df(data_dir=race_data_dir, split=split)
             df_race_c = self.get_raw_race_c_df(data_dir=race_c_data_dir, split=split)
             df = pd.concat([df_race, df_race_c])
-            assert set(df.columns) == set(DF_COLS)
             if save_dataset:
                 df.to_csv(os.path.join(output_data_dir, f'race_pp_{split}.csv'), index=False)
             dataset[split] = df.copy()
         return dataset
 
-    def get_subsampled_racepp_dataset(self, dataset, training_size, output_data_dir, random_state=None):
+    def get_subsampled_racepp_dataset(
+            self,
+            dataset: Dict[str, pd.DataFrame],
+            training_size: int,
+            output_data_dir: str,
+            random_state: int = None,
+    ) -> Dict[str, pd.DataFrame]:
         # TODO:
         #  this is the sampling per class (in the original implementation, it is balanced!!), I should also
         #  implement the sampling "unbalanced"
@@ -62,24 +79,36 @@ class RaceDatamanager(DataManager):
         subsampled_dataset[TEST] = dataset[TEST].copy()
         return subsampled_dataset
 
-    def prepare_race_dataset(self, race_data_dir, output_data_dir):
+    def get_race_dataset(self, data_dir: str, out_data_dir: str, save_dataset: bool = True) -> Dict[str, pd.DataFrame]:
+        dataset = dict()
         for split in [TRAIN, DEV, TEST]:
-            df_race = self.get_raw_race_df(data_dir=race_data_dir, split=split)
-            assert set(df_race.columns) == set(DF_COLS)
-            df_race.to_csv(os.path.join(output_data_dir, f'race_{split}.csv'), index=False)
+            df = self.get_raw_race_df(data_dir=data_dir, split=split)
+            if save_dataset:
+                df.to_csv(os.path.join(out_data_dir, f'race_{split}.csv'), index=False)
+            dataset[split] = df.copy()
+        return dataset
 
-    def prepare_race_c_dataset(self, race_c_data_dir, output_data_dir):
+    def get_race_c_dataset(self, data_dir: str, out_data_dir: str, save_dataset: bool = True) -> Dict[str, pd.DataFrame]:
+        dataset = dict()
         for split in [TRAIN, DEV, TEST]:
-            df_race_c = self.get_raw_race_c_df(data_dir=race_c_data_dir, split=split)
-            assert set(df_race_c.columns) == set(DF_COLS)
-            df_race_c.to_csv(os.path.join(output_data_dir, f'race_c_{split}.csv'), index=False)
+            df = self.get_raw_race_c_df(data_dir=data_dir, split=split)
+            if save_dataset:
+                df.to_csv(os.path.join(out_data_dir, f'race_c_{split}.csv'), index=False)
+            dataset[split] = df.copy()
+        return dataset
 
-    def _append_new_reading_passage_to_df(self, df, reading_passage_data, split, level):
-        answers = reading_passage_data['answers']
-        options = reading_passage_data['options']
-        questions = reading_passage_data['questions']
-        article = reading_passage_data['article']
-        context_id = reading_passage_data['id']
+    def _append_new_reading_passage_to_df(
+            self,
+            df: pd.DataFrame,
+            reading_passage_data,
+            split: str,
+            level: str,
+    ) -> pd.DataFrame:
+        answers = reading_passage_data[self.ANSWERS]
+        options = reading_passage_data[self.OPTIONS]
+        questions = reading_passage_data[self.QUESTIONS]
+        article = reading_passage_data[self.ARTICLE]
+        context_id = reading_passage_data[self.ID]
 
         for idx in range(len(questions)):
             # this is just to check that there are no anomalies
@@ -95,22 +124,24 @@ class RaceDatamanager(DataManager):
                                                CONTEXT_ID: context_id[:-4],
                                                Q_ID: '%s_q%d' % (context_id[:-4], idx),
                                                SPLIT: split,
-                                               DIFFICULTY: self.LEVEL_TO_DIFFICULTY_MAP[level]}])])
+                                               DIFFICULTY: self.LEVEL_TO_INT_DIFFICULTY_MAP[level]}])])
         return df
 
-    def get_raw_race_df(self, data_dir: str, split: str):
+    def get_raw_race_df(self, data_dir: str, split: str) -> pd.DataFrame:
         df = pd.DataFrame(columns=DF_COLS)
         for level in [self.HIGH, self.MIDDLE]:
             for filename in listdir(os.path.join(data_dir, split, level)):
                 with open(os.path.join(data_dir, split, level, filename), 'r') as f:
                     reading_passage_data = json.load(f)
                 df = self._append_new_reading_passage_to_df(df, reading_passage_data, split, level)
+        assert set(df.columns) == set(DF_COLS)
         return df
 
-    def get_raw_race_c_df(self, data_dir: str, split: str):
+    def get_raw_race_c_df(self, data_dir: str, split: str) -> pd.DataFrame:
         df = pd.DataFrame(columns=DF_COLS)
         for filename in listdir(os.path.join(data_dir, split)):
             with open(os.path.join(data_dir, split, filename), 'r') as f:
                 reading_passage_data = json.load(f)
             df = self._append_new_reading_passage_to_df(df, reading_passage_data, split, level=self.COLLEGE)
+        assert set(df.columns) == set(DF_COLS)
         return df

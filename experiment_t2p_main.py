7b2cca2eec618fb1d0ed32f0b85b6228b30b4d53
Luca Benedetto <luca.benedetto93@gmail.com>
2023-01-19 19:06:47 +0000

    refactor all the setup for getting text2props configurations.

--- experiment_t2p_main.py
+++ experiment_t2p_main.py
@@ -1,13 +1,10 @@
-from sklearn.preprocessing import normalize
 from text2props.modules.estimators_from_text import FeatureEngAndRegressionPipeline
-from text2props.modules.feature_engineering import FeatureEngineeringModule
-from text2props.modules.regression import RegressionModule
 
 from qdet_utils.experiment import Text2propsExperiment
 from qdet_utils.text2props_configs import (
     get_config,
-    get_text2props_regression_components_from_config,
-    get_text2props_feat_eng_components_from_config,
+    get_regression_module_from_config,
+    get_feature_engineering_module_from_config,
     get_dict_params_by_config,
 )
 
@@ -26,8 +23,8 @@ experiment.get_dataset()
 difficulty_range = experiment.get_difficulty_range(dataset_name)
 config = get_config(feature_engineering_config=feature_eng_config, regression_config=regression_config)
 feat_eng_and_regr_pipeline = FeatureEngAndRegressionPipeline(
-    FeatureEngineeringModule(get_text2props_feat_eng_components_from_config(config, random_seed), normalize_method=normalize),
-    RegressionModule(get_text2props_regression_components_from_config(config, difficulty_range, random_seed))
+    get_feature_engineering_module_from_config(config, random_seed),
+    get_regression_module_from_config(config, difficulty_range, random_seed)
 )
 experiment.init_model(None, config, feat_eng_and_regr_pipeline)
 
--- qdet_utils/text2props_configs.py
+++ qdet_utils/text2props_configs.py
@@ -1,5 +1,26 @@
 from scipy.stats import randint
 from text2props.constants import DIFFICULTY
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import normalize
+
+from text2props.model import Text2PropsModel
+from text2props.modules.estimators_from_text import (
+    FeatureEngAndRegressionPipeline,
+    FeatureEngAndRegressionEstimatorFromText,
+)
+from text2props.modules.feature_engineering import FeatureEngineeringModule
+from text2props.modules.feature_engineering.components import (
+    ReadabilityFeaturesComponent,
+    IRFeaturesComponent,
+    LinguisticFeaturesComponent,
+    Word2VecFeaturesComponent,
+)
+from text2props.modules.feature_engineering.utils import vectorizer_text_preprocessor as prepr
+from text2props.modules.latent_traits_calibration import KnownParametersCalibrator
+from text2props.modules.regression import RegressionModule
+from text2props.modules.regression.components import SklearnRegressionComponent
 
 
 def get_dict_params_by_config(config):
@@ -10,6 +31,7 @@ def get_dict_params_by_config(config):
         return {DIFFICULTY: [{'regressor__n_estimators': randint(20, 200), 'regressor__max_depth': randint(2, 50)}]}
     else:
         raise NotImplementedError
+
 
 def get_config(feature_engineering_config, regression_config):
     assert feature_engineering_config in FEATURE_ENGINEERING_CONFIGS
@@ -59,3 +81,63 @@ FEATURE_ENGINEERING_CONFIGS = {
 LR = 'LR'
 RF = 'RF'
 REGRESSION_CONFIGS = {LR, RF}
+
+
+def get_feature_engineering_module_from_config(config, seed) -> FeatureEngineeringModule:
+    feature_engineering_config = config.split('__')[0]
+    # All the possible components
+    linguistic_features = LinguisticFeaturesComponent(version=2)
+    readability_features = ReadabilityFeaturesComponent(use_smog=False, version=2)
+    w2v_q_only = Word2VecFeaturesComponent(size=100, concatenate_correct=False, concatenate_wrong=False, seed=seed)
+    w2v_q_all = Word2VecFeaturesComponent(size=100, concatenate_correct=True, concatenate_wrong=True, seed=seed)
+    w2v_q_correct = Word2VecFeaturesComponent(size=100, concatenate_correct=True, concatenate_wrong=False, seed=seed)
+    tfidf_q_only = IRFeaturesComponent(
+        TfidfVectorizer(stop_words='english', preprocessor=prepr, min_df=0.05, max_df=0.95, max_features=1000),
+        concatenate_correct=False,
+        concatenate_wrong=False,
+    )
+    tfidf_q_correct = IRFeaturesComponent(
+        TfidfVectorizer(stop_words='english', preprocessor=prepr, min_df=0.05, max_df=0.95, max_features=1000),
+        concatenate_correct=True,
+        concatenate_wrong=False,
+    )
+    tfidf_q_all = IRFeaturesComponent(
+        TfidfVectorizer(stop_words='english', preprocessor=prepr, min_df=0.05, max_df=0.95, max_features=1000),
+        concatenate_correct=True,
+        concatenate_wrong=True,
+    )
+    # the combinations
+    dict_feat_eng_components = {
+        LING: [linguistic_features],
+        READ: [readability_features],
+        W2V_Q_ONLY: [w2v_q_only],
+        W2V_Q_ALL: [w2v_q_all],
+        W2V_Q_CORRECT: [w2v_q_correct],
+        LING_AND_READ: [linguistic_features, readability_features],
+        W2V_Q_ONLY_AND_LING: [w2v_q_only, linguistic_features],
+        W2V_Q_ALL_AND_LING: [w2v_q_all, linguistic_features],
+        W2V_Q_CORRECT_AND_LING: [w2v_q_correct, linguistic_features],
+        LING_AND_READ_AND_R2DE_Q_ONLY: [linguistic_features, tfidf_q_only],  # TODO !
+        LING_AND_READ_AND_R2DE_Q_CORRECT: [linguistic_features, tfidf_q_correct],  # TODO !
+        LING_AND_READ_AND_R2DE_Q_ALL: [linguistic_features, tfidf_q_all],  # TODO !
+        W2V_Q_ONLY_AND_LING_AND_R2DE_Q_ONLY: [w2v_q_only, linguistic_features, tfidf_q_only],
+        W2V_Q_ONLY_AND_LING_AND_R2DE_Q_CORRECT: [w2v_q_only, linguistic_features, tfidf_q_correct],
+        W2V_Q_ONLY_AND_LING_AND_R2DE_Q_ALL: [w2v_q_only, linguistic_features, tfidf_q_all],
+        W2V_Q_ALL_AND_LING_AND_R2DE_Q_ONLY: [w2v_q_all, linguistic_features, tfidf_q_only],
+        W2V_Q_ALL_AND_LING_AND_R2DE_Q_CORRECT: [w2v_q_all, linguistic_features, tfidf_q_correct],
+        W2V_Q_ALL_AND_LING_AND_R2DE_Q_ALL: [w2v_q_all, linguistic_features, tfidf_q_all],
+        W2V_Q_CORRECT_AND_LING_AND_R2DE_Q_ONLY: [w2v_q_correct, linguistic_features, tfidf_q_only],
+        W2V_Q_CORRECT_AND_LING_AND_R2DE_Q_CORRECT: [w2v_q_correct, linguistic_features, tfidf_q_correct],
+        W2V_Q_CORRECT_AND_LING_AND_R2DE_Q_ALL: [w2v_q_correct, linguistic_features, tfidf_q_all],
+    }
+    # Returns the feature engineering module
+    return FeatureEngineeringModule(dict_feat_eng_components[feature_engineering_config], normalize_method=normalize)
+
+
+def get_regression_module_from_config(config, difficulty_range, seed) -> RegressionModule:
+    regression_config = config.split('__')[1]
+    dict_regression_components = {
+        LR: [SklearnRegressionComponent(LinearRegression(), latent_trait_range=difficulty_range)],
+        RF: [SklearnRegressionComponent(RandomForestRegressor(random_state=seed), latent_trait_range=difficulty_range)],
+    }
+    return RegressionModule(dict_regression_components[regression_config])

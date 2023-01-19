c35528073ca0164d049c5cfa772b87c6782bd509
Luca Benedetto <luca.benedetto93@gmail.com>
2023-01-19 19:12:53 +0000

    add the if name == main

--- experiment_t2p_main.py
+++ experiment_t2p_main.py
@@ -17,18 +17,19 @@ from experiment_t2p_params import (
     n_jobs,
 )
 
-experiment = Text2propsExperiment(dataset_name=dataset_name, random_seed=random_seed)
-experiment.get_dataset()
+if __name__ == "__main__":
+    experiment = Text2propsExperiment(dataset_name=dataset_name, random_seed=random_seed)
+    experiment.get_dataset()
 
-difficulty_range = experiment.get_difficulty_range(dataset_name)
-config = get_config(feature_engineering_config=feature_eng_config, regression_config=regression_config)
-feat_eng_and_regr_pipeline = FeatureEngAndRegressionPipeline(
-    get_feature_engineering_module_from_config(config, random_seed),
-    get_regression_module_from_config(config, difficulty_range, random_seed)
-)
-experiment.init_model(None, config, feat_eng_and_regr_pipeline)
+    difficulty_range = experiment.get_difficulty_range(dataset_name)
+    config = get_config(feature_engineering_config=feature_eng_config, regression_config=regression_config)
+    feat_eng_and_regr_pipeline = FeatureEngAndRegressionPipeline(
+        get_feature_engineering_module_from_config(config, random_seed),
+        get_regression_module_from_config(config, difficulty_range, random_seed)
+    )
+    experiment.init_model(None, config, feat_eng_and_regr_pipeline)
 
-dict_params = get_dict_params_by_config(config)
-experiment.train(dict_params, n_iter, n_jobs)
-experiment.predict()
-experiment.evaluate()
+    dict_params = get_dict_params_by_config(config)
+    experiment.train(dict_params, n_iter, n_jobs)
+    experiment.predict()
+    experiment.evaluate()

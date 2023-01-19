bb5891dfbc9a8f2e1ac9484a8f44dfb1aafd7b89
Luca Benedetto <luca.benedetto93@gmail.com>
2023-01-19 18:28:34 +0000

    rename qdet_utils.text2props_configs

--- results_get_metrics_dataframe.py
+++ results_get_metrics_dataframe.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 from qdet_utils.constants import RACE_PP, ARC, ARC_BALANCED, AM, OUTPUT_DIR, RACE_PP_4K, RACE_PP_8K, RACE_PP_12K
 from qdet_utils.constants import LIST_TF_ENCODINGS, TF_MODELS, TF_Q_ONLY
-from qdet_utils.configs import *
+from qdet_utils.text2props_configs import *
 from qdet_utils.scripts_utils import METRICS
 
 

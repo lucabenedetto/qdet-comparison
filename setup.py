e1720281188a6f19f5dc4415aefd8a987cd7fb9d
Luca Benedetto <luca.benedetto93@gmail.com>
2023-01-20 18:20:37 +0000

    add requirements pyyaml

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
         "numpy==1.23.4",
         "pandas==1.5.1",
         "pyirt==0.3.4",
+        "PyYAML==6.0",
         "scikit-learn==1.1.3",
         "scipy==1.9.3",
         "seaborn==0.12.1",

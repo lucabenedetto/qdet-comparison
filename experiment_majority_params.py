0ac48df15a4b7d883ba32632c9c000543b1ac860
Luca Benedetto <luca.benedetto93@gmail.com>
2023-01-20 10:24:38 +0000

    remove unused import and constants

--- experiment_majority_params.py
+++ experiment_majority_params.py
@@ -1,4 +1,3 @@
-from scipy.stats import randint
 from qdet_utils.constants import (
     RACE_PP,
     RACE_PP_4K,
@@ -8,8 +7,6 @@ from qdet_utils.constants import (
     ARC_BALANCED,
     AM,
 )
-
-LIST_DATASET_NAMES = [RACE_PP, ARC, ARC_BALANCED, AM, RACE_PP_4K, RACE_PP_8K, RACE_PP_12K]
 
 dataset_name = ARC_BALANCED
 model_name = 'majority'

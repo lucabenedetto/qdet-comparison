99778cd39e9e0adb3113ce6d6dc6c86c5a351ace
Luca Benedetto <luca.benedetto93@gmail.com>
2023-03-06 12:07:45 +0000

    refator by renaming config variables for clarity

--- experiment_main.py
+++ experiment_main.py
@@ -16,12 +16,12 @@ from qdet_utils.text2props_configs import (
 from qdet_utils.r2de_configs import r2de_get_dict_params_by_dataset
 
 
-def main(config):
-    model_type = config['model_type']
-    dataset_name = config['dataset_name']
+def main(experiment_config):
+    model_type = experiment_config['model_type']
+    dataset_name = experiment_config['dataset_name']
 
     if model_type == 'majority':
-        model_name = config['model_name'] if config['model_name'] != 'None' else 'majority'
+        model_name = experiment_config['model_name'] if experiment_config['model_name'] != 'None' else 'majority'
         experiment = MajorityExperiment(dataset_name=dataset_name)
         experiment.get_dataset()
         experiment.init_model(None, model_name, None)
@@ -30,10 +30,10 @@ def main(config):
         experiment.evaluate(compute_correlation=False)
         return
 
-    random_seed = config['random_seed'] if config['random_seed'] != 'None' else None
+    random_seed = experiment_config['random_seed'] if experiment_config['random_seed'] != 'None' else None
 
     if model_type == 'random':
-        model_name = config['model_name'] if config['model_name'] != 'None' else 'random'
+        model_name = experiment_config['model_name'] if experiment_config['model_name'] != 'None' else 'random'
         experiment = RandomExperiment(dataset_name=dataset_name, random_seed=random_seed)
         experiment.get_dataset()
         experiment.init_model(None, model_name, None)
@@ -42,13 +42,13 @@ def main(config):
         experiment.evaluate()
         return
 
-    n_iter = config['n_iter']
-    n_jobs = config['n_jobs']
-    cv = config['cv']
+    n_iter = experiment_config['n_iter']
+    n_jobs = experiment_config['n_jobs']
+    cv = experiment_config['cv']
 
     if model_type == 'r2de':
-        encoding_idx = config['encoding_idx']
-        model_name = config['model_name'] if config['model_name'] != 'None' else f'r2de_encoding{encoding_idx}'
+        encoding_idx = experiment_config['encoding_idx']
+        model_name = experiment_config['model_name'] if experiment_config['model_name'] != 'None' else f'r2de_encoding{encoding_idx}'
         experiment = R2deExperiment(dataset_name=dataset_name, random_seed=random_seed)
         experiment.get_dataset()
         experiment.init_model(model_name=model_name, encoding_idx=encoding_idx)
@@ -59,19 +59,19 @@ def main(config):
         return
 
     if model_type == 'text2props':
-        feature_eng_config = config['feature_eng_config']
-        regression_config = config['regression_config']
+        feature_eng_config = experiment_config['feature_eng_config']
+        regression_config = experiment_config['regression_config']
         experiment = Text2propsExperiment(dataset_name=dataset_name, random_seed=random_seed)
         experiment.get_dataset()
         difficulty_range = experiment.get_difficulty_range(dataset_name)  # TODO this should probably be somewhere else.
-        config = text2props_get_config(feature_engineering_config=feature_eng_config, regression_config=regression_config)
+        model_config = text2props_get_config(feature_engineering_config=feature_eng_config, regression_config=regression_config)
         feat_eng_and_regr_pipeline = FeatureEngAndRegressionPipeline(
-            text2props_get_feature_engineering_module_from_config(config, random_seed),
-            text2props_get_regression_module_from_config(config, difficulty_range, random_seed)
+            text2props_get_feature_engineering_module_from_config(model_config, random_seed),
+            text2props_get_regression_module_from_config(model_config, difficulty_range, random_seed)
         )
-        model_name = config['model_name'] if config['model_name'] != 'None' else config
+        model_name = experiment_config['model_name'] if experiment_config['model_name'] != 'None' else model_config
         experiment.init_model(model_name=model_name, feature_eng_and_regression_pipeline=feat_eng_and_regr_pipeline)
-        dict_params = text2props_get_dict_params_by_config(config)
+        dict_params = text2props_get_dict_params_by_config(model_config)
         experiment.train(dict_params, n_iter, n_jobs, cv=cv)
         experiment.predict()
         experiment.evaluate()

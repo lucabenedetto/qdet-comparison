20dba2e384271b28650459c3ac22665496e141b6
Luca Benedetto <luca.benedetto93@gmail.com>
2023-04-12 12:40:01 +0100

    add import of transformers experiment

--- qdet_utils/experiment/__init__.py
+++ qdet_utils/experiment/__init__.py
@@ -1,5 +1,6 @@
 from ._experiment import BaseExperiment
 from ._r2de_experiment import R2deExperiment
 from ._text2props_experiment import Text2propsExperiment
+from ._transformer_experiment import TransformerExperiment
 from ._majority import MajorityExperiment
 from ._random import RandomExperiment

3bfd877c5bb86c700be881de3778ae78e88e6756
Luca Benedetto <luca.benedetto93@gmail.com>
2023-04-12 10:37:27 +0100

    add possibility of specifying dict params

--- qdet_utils/r2de_configs.py
+++ qdet_utils/r2de_configs.py
@@ -5,14 +5,22 @@ from qdet_utils.constants import (
 )
 
 
-def r2de_get_dict_params_by_dataset(dataset_name):
-    param_distribution = {'tfidf__max_features': randint(100, 2000),
-                          'regressor__max_depth': randint(2, 50),
-                          'regressor__n_estimators': randint(2, 200), }
+def r2de_get_dict_params_by_dataset(dataset_name, param_distribution=None):
+    if param_distribution is not None:
+        return param_distribution
     if dataset_name in {ARC, ARC_BALANCED}:
-        param_distribution['tfidf__max_df'] = [0.95, 1.0]
-        param_distribution['tfidf__min_df'] = [1, 0.05, 0.1]
+        return {
+            'tfidf__max_features': randint(100, 2000),
+            'regressor__max_depth': randint(2, 50),
+            'regressor__n_estimators': randint(2, 200),
+            'tfidf__max_df': [0.95, 1.0],
+            'tfidf__min_df': [1, 0.05, 0.1],
+        }
     else:
-        param_distribution['tfidf__max_df'] = [0.8, 0.85, 0.9, 0.95, 1.0]
-        param_distribution['tfidf__min_df'] = [1, 0.05, 0.1, 0.15, 0.2]
-    return param_distribution
+        return {
+            'tfidf__max_features': randint(100, 2000),
+            'regressor__max_depth': randint(2, 50),
+            'regressor__n_estimators': randint(2, 200),
+            'tfidf__max_df': [0.8, 0.85, 0.9, 0.95, 1.0],
+            'tfidf__min_df': [1, 0.05, 0.1, 0.15, 0.2],
+        }
